4b6e40f7e79055dff249fc75e5c3e9d5286067fb
fmeulen <f.h.vandermeulen@tudelft.nl>
2022-12-20 17:28:03 +0100

    more_exp

--- src/ex_abffg_finitestate3.jl
+++ src/ex_abffg_finitestate3.jl
@@ -10,14 +10,13 @@ using QuadGK
 
 include("funcdefs.jl")
 
-EASYkernel = false
+EASYkernel = true
 TESTING = false # if true: observe fully with known root node
 
 # Need to specify
 # Λ = observation kernel
 # Πroot = prior on initial state
 # Ki = transition kernel on hidden state
-
 
 # Define the state space
 E = [1, 2, 3]
@@ -26,25 +25,30 @@ E = [1, 2, 3]
 N = 100
 
 if EASYkernel
-        Ki(θ) = [[1.0 - 0.5θ 0.25θ   0.25θ  ]
-         [0   1.0 - θ θ  ]
-         [0.4θ   0.3θ   1.0 - 0.7θ]]
+
+         Ki(θ) = [1.0 - 0.5θ[1] 0.25θ[1]   0.25θ[1]  ;
+         0   1.0 - θ[1] θ[1]  
+         0.4θ   0.3θ[1]   1.0 - 0.7θ[1]]
+
 else
-        Ki(θ) = [1.0 - θ  θ   0.0  ;
+        Ki(θ) = [1.0 - θ.p  θ.p   0.0  ;
         0.25   0.5  0.25  ;
         0.4   0.3   0.3]
 end
 
 if TESTING    # if perfect observations, with known root node, we should be able to perfectly reconstruct 
      Λ = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+     Λi(θ) = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
      Πroot = [1.0, 0.0, 0.0]
 else 
      Λ = [1.0 0.0; 1.0 0.0; 0.0 1.0]
+     Λi(θ) = [1.0 0.0; 1.0 0.0; 0.0 1.0]
      Πroot = [1.0, 1.0, 1.0]/3.0
 end
 
 # True parameter vector
-θ0 = 0.3
+θ0 = ComponentArray(p=0.3)
+#θ0 =[0.3]
 
 # Hidden Markov chain ransition kernel
 K = Ki(θ0)
@@ -53,7 +57,8 @@ K = Ki(θ0)
 
 # generate track, and sample from conditioned process
 xs, ys = generate_track(K, Λ, Πroot, N)
-l, hs = likelihood_and_guided(K, Λ, Πroot, ys)
+#l, hs = likelihood_and_guided(K, Λ, Πroot, ys)
+(l, hs) = loglik_and_bif(θ0, Πroot, ys)
 xstars = guided_track(K, Πroot, hs)
 # plotting 
 ts = 0:N
@@ -70,15 +75,21 @@ pl_paths
 ############ ------------------------------------------------ ############
 
 # compute mle and plot with loglikelihood
-Θgrid = collect(0:0.01:1)
-pl_lik = plot(Θgrid, negloglik(Λ, Πroot, ys).(Θgrid), label="log-likelihood")
-out = optimize(negloglik(Λ, Πroot, ys), 0.0, 1.0)    # box constrained optimization
-print(out)
+grid = 0:0.01:1
+θgrid = [ComponentArray(p=x) for x in grid]
+#θgrid = [x for x in grid]
+pl_lik = plot(grid, negloglik(Πroot, ys).(Θgrid), label="neg. loglikelihood")
+
+
+negloglik(Πroot, ys)(θgrid[20])
+∇negloglik(Πroot, ys)(θgrid[20])
+
+out = optimize(negloglik(Πroot, ys), 0.0, 1.0)    # box constrained optimization
 vline!(pl_lik, [out.minimizer], label="mle")
 vline!(pl_lik, [θ0], label="true")
-pl_lik
-# check that derivative is zero at optimum (only works with first defn of negloklik). Why??
-∇negloglik(Λ, Πroot, ys)(out.minimizer)
+
+
+
 
 ############ ------------------------------------------------ ############
 
--- src/funcdefs.jl
+++ src/funcdefs.jl
@@ -7,70 +7,120 @@ K: kernel on latent state (matrix)
 Πroot: prior on x0 (root node is like x_{-1}), represented by a column vector
 
 """
-function generate_track(K, Λ, Πroot, N)       # Generate exact track + observations
-    xprev = sample(Weights(Πroot))  # sample x0
+function generate_track(K, Λ, Πroot, N)             # Generate exact track + observations
+    xprev = sample(Weights(Πroot))                  # sample x0
     xs = [xprev]
     ys = [sample(Weights(Λ[xprev,:]))]
-
     for i=1:N
-            x = sample(Weights(K[xprev,:]))         # Generate sample from previous state
-            push!(xs, x)
-            y = sample(Weights(Λ[x,:]))             # Generate observation from sample
-            push!(ys, y)
-            xprev = x
+        x = sample(Weights(K[xprev,:]))         # Generate sample from previous state
+        push!(xs, x)
+        y = sample(Weights(Λ[x,:]))             # Generate observation from sample
+        push!(ys, y)
+        xprev = x
     end
     (xs, ys)
 end
 
-function likelihood_and_guided(K, Λ, Πroot, ys)
-    N = length(ys) - 1
-    hprev = Λ[:,ys[N+1]]                   # Terminal h-transform is just pullback from leaf
-    hs = [hprev]
-
-    for i=N:-1:1
-            h = K * hprev .* Λ[:,ys[i]]  # Pullback from leaf .* pullback from observation
-            push!(hs, h)
-            hprev = h
-    end
-    # hroot = K* hprev # should not be done!!!
-    # hs = reverse(hs)                     # Reverse list for correctly indexed guiding terms
-    # likelihood = proot' * hroot          # likelihood = \int h_0(x_0) p(x_0) dx_0
-
-    #hroot = K* hprev
-    hs = reverse(hs)                     # Reverse list for correctly indexed guiding terms
-    likelihood = Πroot' * hprev          # likelihood = \int h_0(x_0) p(x_0) dx_0
-
-    (likelihood, hs)
+function normalise!(x)
+    s = sum(x)
+    x .= x/s
+    log(s)
 end
 
-function likelihood(θ, Λ, Πroot, ys)
+function loglik_and_bif(θ, Πroot, ys)
     N = length(ys) - 1
     K = Ki(θ)
-    hprev = Λ[:,ys[N+1]]                   
+    Λ = Λi(θ)
+    hprev = Λ[:,ys[N+1]]     
+    hs = [hprev]
+    loglik = zero(θ[1])
     for i=N:-1:1
-            h = K * hprev .* Λ[:,ys[i]]  
+            h = (K * hprev) .* Λ[:,ys[i]]  
+            c = normalise!(h)
+            loglik += c
+            pushfirst!(hs, h)
             hprev = h
+            
     end
-    Πroot' * hprev          
+    loglik += log(Πroot' * hprev)
+    (ll=loglik, h=hs)          
 end
+
+negloglik(Πroot, ys) = (θ) ->  -loglik_and_bif(θ, Πroot, ys).ll
+∇negloglik(Πroot, ys) = (θ) -> ForwardDiff.gradient(negloglik(Πroot, ys), θ)
 
 function guided_track(K, Πroot, hs)# Generate approximate track
     N = length(hs) - 1
     xprev = sample(Weights(Πroot .* hs[1])) # Weighted prior distribution
-
-    xstars = [xprev]
-
+    xᵒ = [xprev]
     for i=1:N
             p = K[xprev,:] .* hs[i+1]         # Weighted transition density
             x = sample(Weights(p))
-            push!(xstars, x)
+            push!(xᵒ, x)
             xprev = x
     end
-    xstars
+    xᵒ
 end
 
 
-negloglik(Λ, Πroot, ys) = (θ) ->  -log(likelihood(θ, Λ, Πroot, ys))
-#negloglik(Λ, Πroot, ys) = (θ) ->  -log(likelihood_and_guided(Ki(θ), Λ, Πroot, ys)[1])  # autodiff does not work on this one
 
-∇negloglik(Λ, Πroot, ys) = (θ) -> ForwardDiff.derivative(negloglik(Λ, Πroot, ys), θ)
+
+    
+# function likelihood_and_guided(K, Λ, Πroot, ys)
+#     N = length(ys) - 1
+#     hprev = Λ[:,ys[N+1]]                   # Terminal h-transform is just pullback from leaf
+#     hs = [hprev]
+
+#     for i=N:-1:1
+#             h = K * hprev .* Λ[:,ys[i]]  # Pullback from leaf .* pullback from observation
+#             push!(hs, h)
+#             hprev = h
+#     end
+#     # hroot = K* hprev # should not be done!!!
+#     # hs = reverse(hs)                     # Reverse list for correctly indexed guiding terms
+#     # likelihood = proot' * hroot          # likelihood = \int h_0(x_0) p(x_0) dx_0
+
+#     #hroot = K* hprev
+#     hs = reverse(hs)                     # Reverse list for correctly indexed guiding terms
+#     likelihood = Πroot' * hprev          # likelihood = \int h_0(x_0) p(x_0) dx_0
+
+#     (likelihood, hs)
+# end
+
+# function loglikelihood_wrong(θ, Πroot, ys)
+# # wrong implementation, underflow risk
+#     N = length(ys) - 1
+#     K = Ki(θ)
+#     Λ = Λi(θ)
+#     hprev = Λ[:,ys[N+1]]     
+#     for i=N:-1:1
+#             hprev = (K * hprev) .* Λ[:,ys[i]]  
+#             @show hprev
+#     end
+#     log(Πroot' * hprev)          
+# end
+
+
+
+# negloglikelihood2(Πroot, ys) = (θ) ->  -loglikelihood2(θ, Πroot, ys)
+
+# function guided_track(K, Πroot, hs)# Generate approximate track
+#     N = length(hs) - 1
+#     xprev = sample(Weights(Πroot .* hs[1])) # Weighted prior distribution
+
+#     xstars = [xprev]
+
+#     for i=1:N
+#             p = K[xprev,:] .* hs[i+1]         # Weighted transition density
+#             x = sample(Weights(p))
+#             push!(xstars, x)
+#             xprev = x
+#     end
+#     xstars
+# end
+
+
+# negloglik(Πroot, ys) = (θ) ->  -log(likelihood(θ, Πroot, ys))
+# #negloglik(Λ, Πroot, ys) = (θ) ->  -log(likelihood_and_guided(Ki(θ), Λ, Πroot, ys)[1])  # autodiff does not work on this one
+
+# ∇negloglik(Πroot, ys) = (θ) -> ForwardDiff.gradient(negloglik(Πroot, ys), θ)
